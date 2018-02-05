016f8e53cc1f6348ef6d29ccadec5df35d4110d7
Administrator <13025166246@163.com>
2018-02-05 10:06:57 +0800

    add pic

--- app/src/main/java/com/example/zy/androidgithubuse/LaunchActivity.java
+++ app/src/main/java/com/example/zy/androidgithubuse/LaunchActivity.java
@@ -2,6 +2,7 @@ package com.example.zy.androidgithubuse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class LaunchActivity extends AppCompatActivity {
@@ -9,8 +10,17 @@ public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
-        startActivity(new Intent(this, MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                redirectTo();
+            }
+        }, 2000);
+    }
+
+    private void redirectTo() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
         finish();
     }
 }
